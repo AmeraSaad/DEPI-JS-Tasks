1d9392a003303fd97c5900a8f61a741aa2146953
Amera <amera@gmail.com>
2024-07-23 20:51:25 +0300

    Cart Added

--- E-Shop-JQuery/js/script.js
+++ E-Shop-JQuery/js/script.js
@@ -8,6 +8,10 @@ const itemsElement = $("#items");
 const searchInput = $("#search-input");
 const searchButton = $("#search-button");
 const categorySelect = $("#category-select");
+const itemsCount = $("#items-count");
+const cartItemsContainer = $("#cart-items");
+const emptyCartMessage = $("#empty-cart-message");
+
 let cartItems = [];
 
 function startLoading() {
@@ -43,7 +47,6 @@ function fetchCategories() {
       mainElement.removeClass("d-none");
       mainElement.addClass("row");
 
-      // Assuming each category is an object with a 'name' property
       categoriesContainer.html(
         data.map((item) => `<li class="category-item" data-category="${item.name}">${item.name}</li>`).join("")
       );
@@ -60,7 +63,6 @@ function fetchCategories() {
     stopLoading
   );
 }
-
 
 function fetchProducts(query = "", category = "") {
   let endpoint = "products";
@@ -87,7 +89,7 @@ function fetchProducts(query = "", category = "") {
                 <h5 class="card-title">${item.title}</h5>
                 <p class="card-text">${item.description}</p>
                 <div class="price">$${item.price}</div>
-                <button class="btn add-to-cart" data-id="${item.id}">Add to cart</button>
+                <button class="btn add-to-cart" data-id="${item.id}" data-item='${JSON.stringify(item)}'>Add to cart</button>
               </div>
             </div>
           </div>
@@ -100,6 +102,56 @@ function fetchProducts(query = "", category = "") {
     startLoading,
     stopLoading
   );
+}
+
+function updateCartDisplay() {
+  itemsCount.text(cartItems.length);
+  if (cartItems.length > 0) {
+    itemsCount.removeClass("d-none");
+    emptyCartMessage.addClass("d-none");
+  } else {
+    emptyCartMessage.removeClass("d-none");
+  }
+}
+
+function renderCartItems() {
+  if (cartItems.length === 0) {
+    emptyCartMessage.removeClass("d-none");
+    cartItemsContainer.html("");
+  } else {
+    emptyCartMessage.addClass("d-none");
+    cartItemsContainer.html(
+      cartItems.map((item, index) => `
+        <li class="cart-item d-flex justify-content-between align-items-center mb-2">
+          <div>
+            <h5>${item.title}</h5>
+            <p>$${item.price}</p>
+            <div class="d-flex align-items-center">
+              <button class="btn btn-secondary btn-sm decrease-quantity" data-index="${index}">-</button>
+              <span class="mx-2">${item.quantity}</span>
+              <button class="btn btn-secondary btn-sm increase-quantity" data-index="${index}">+</button>
+            </div>
+          </div>
+          <button class="btn btn-danger btn-sm remove-item" data-index="${index}">Remove</button>
+        </li>
+      `).join("")
+    );
+  }
+}
+
+function addToCart(item) {
+  const existingItem = cartItems.find(cartItem => cartItem.id === item.id);
+  if (existingItem) {
+    if (existingItem.quantity < item.stock) {
+      existingItem.quantity += 1;
+    } else {
+      alert('Cannot add more items than available in stock.');
+    }
+  } else {
+    cartItems.push({ ...item, quantity: 1 });
+  }
+  updateCartDisplay();
+  renderCartItems();
 }
 
 searchButton.on("click", function () {
@@ -122,7 +174,39 @@ categoriesContainer.on("click", ".category-item", function () {
   fetchProducts("", category);
 });
 
+itemsElement.on("click", ".add-to-cart", function () {
+  const item = JSON.parse($(this).attr('data-item'));
+  addToCart(item);
+});
+
+cartItemsContainer.on("click", ".remove-item", function () {
+  const index = $(this).data("index");
+  cartItems.splice(index, 1);
+  updateCartDisplay();
+  renderCartItems();
+});
+
+cartItemsContainer.on("click", ".increase-quantity", function () {
+  const index = $(this).data("index");
+  if (cartItems[index].quantity < cartItems[index].stock) {
+    cartItems[index].quantity += 1;
+  } else {
+    alert('Cannot add more items than available in stock.');
+  }
+  updateCartDisplay();
+  renderCartItems();
+});
+
+cartItemsContainer.on("click", ".decrease-quantity", function () {
+  const index = $(this).data("index");
+  if (cartItems[index].quantity > 1) {
+    cartItems[index].quantity -= 1;
+  }
+  updateCartDisplay();
+  renderCartItems();
+});
 
 // Initialize
+itemsCount.removeClass("d-none");  // Show the item count at the beginning
 fetchCategories();
 fetchProducts();
